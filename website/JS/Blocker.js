86a3132a0fd75bde24dadddd32d036dc3415547e
Mirai <brandonhicks749@gmail.com>
2018-05-04 23:10:16 -0400

    Update 0.0.4-1
    Adding Javascript NSFW page blocker and setting up the page to goto if consent isnt given

---
+++ website/JS/Blocker.js
@@ -0,0 +1,5 @@
+// This Script Blocks A Page From Loading Until The User Says OK!
+if (window.confirm("If you click OK, You are giving consent that you would like to see NSFW content"))
+{
+window.location.href="LewdsRedirect.html";
+};
